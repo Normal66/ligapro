fa101fdaf5588e2c718e6b450015fc26bd2fb142
Constantin Sidorov <normal66@yandex.ru>
2021-11-20 09:52:03 +0500

    Добавлена ф-ция формирования списка url по дням за период

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from datetime import *
+from dateutil.relativedelta import *
+import calendar
+from dateutil.rrule import *
+from dateutil.parser import *
+from datetime import *
+
+PERIOD = 3        # За сколько месяцев брать матчи
+TOURS_URL = 'https://tt.sport-liga.pro/tours/'
+
+
+def make_list_url_by_day() -> list:
+    # Формируем список url по дням за период с TODAY-PERIOD по TODAY ввиде:
+    # https://tt.sport-liga.pro/tours/?year=2021&month=11&day=4
+    _result = []
+    _tmp = list(list(rrule(DAILY, dtstart=date.today()+relativedelta(months=-PERIOD), until=date.today())))
+    for _items in _tmp:
+        _result.append(f'{TOURS_URL}?year={_items.year}&month={_items.month}&day={_items.day}')
+    return _result
+
+
 if __name__ == '__main__':
-    pass
+    print(make_list_url_by_day())
